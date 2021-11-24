7a85f93c20bf224ff88895755edd5aa3a8740c0d
shadowinhaze <shadowinhaze@gmail.com>
2021-11-25 00:50:50 +0100

    feat: add beauty for loader

--- art-quiz/src/core/components/loader.js
+++ art-quiz/src/core/components/loader.js
@@ -4,6 +4,18 @@ export default class Loader {
     static startLoading() {
         Loader.loaderEl = document.createElement('div')
         Loader.loaderEl.classList.add('loader')
+        Loader.loaderEl.innerHTML = `
+        <div class="circles">
+            <span class="circle circle-1"></span>
+            <span class="circle circle-2"></span>
+            <span class="circle circle-3"></span>
+            <span class="circle circle-4"></span>
+            <span class="circle circle-5"></span>
+            <span class="circle circle-6"></span>
+            <span class="circle circle-7"></span>
+            <span class="circle circle-8"></span>
+        </div>
+        `
         document.body.appendChild(Loader.loaderEl);
     }
 
